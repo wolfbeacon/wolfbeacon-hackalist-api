cbcfc874a7c8c9832de4511565be09ebd9f7b5c0
bholagabbar <dev.shreyans96@gmail.com>
2017-03-08 01:41:03 +0530

    Changed Crud to JpaRepository

--- src/main/java/com/wolfbeacon/dao/HackathonDao.java
+++ src/main/java/com/wolfbeacon/dao/HackathonDao.java
@@ -1,10 +1,11 @@
 package com.wolfbeacon.dao;
 
 import com.wolfbeacon.model.Hackathon;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface HackathonDao extends CrudRepository<Hackathon, Long>, HackathonDaoExtension {
+public interface HackathonDao extends JpaRepository<Hackathon, Long>, HackathonDaoExtension {
 
 }
