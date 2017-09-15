00718352a6dcc8623a3bbd5113b2db5c3859a57a
bholagabbar <dev.shreyans96@gmail.com>
2017-09-16 00:33:28 +0530

    Added apidoc String

--- src/main/java/com/wolfbeacon/api/HackalistHackathonController.java
+++ src/main/java/com/wolfbeacon/api/HackalistHackathonController.java
@@ -15,6 +15,49 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
+
+/**
+ * @api {get} /hackalist-hackathons/ Get Hackalist Hackathons
+ * @apiName GetHackalistHackathons
+ * @apiGroup Hackalist
+ * 
+ * @apiParam {String} start-date (yyyy-MM-dd) Returns all the hackathons after this date. Eg: start-date=2015-10-10 Users unique ID.
+ * @apiParam {String} end-date (yyyy-MM-dd) Returns all the hackathons before this date. Eg: end-date=2015-12-10
+ * @apiParam {String} sort-by=distance&latitude=19.1231&longitude=45.1231 Sorts all the Hackathons in ascending radial order from the coordinates
+ * @apiParam {String} sort-by=date Sorts all the Hackathons in ascending order of start-date. Eg: sort-by=date
+ *
+ * @apiSampleRequest curl -X GET "https://wolfbeacon.com/api/hackalist-hackathons?start-date=2016-01-01&end-date=2016-01-30&sort-by=distance&latitude=40.7127837&longitude=-74.00594130000002"
+ * 
+ * @apiSuccessExample {json} Success-Response:
+ * {
+ * ...
+ *   {
+ *   "id": 20160916552226390,
+ *   "title": "Hack the North",
+ *   "eventLink": "https://hackthenorth.com/",
+ *   "startDate": "2016-09-15",
+ *   "endDate": "2016-09-17",
+ *   "lastUpdatedDate": "2016-11-13T00:00:00.000+0000",
+ *   "year": 2016,
+ *   "location": "Waterloo, ON, Canada",
+ *   "host": "Hack the North",
+ *   "length": 36,
+ *   "size": "1000",
+ *   "travel": true,
+ *   "prize": true,
+ *   "highSchoolers": true,
+ *   "cost": "free",
+ *   "facebookLink": "https://www.facebook.com/hackthenorth",
+ *   "twitterLink": "https://twitter.com/hackthenorth",
+ *   "googlePlusLink": "",
+ *   "imageLink": "https://scontent.xx.fbcdn.net/v/t1.0-1/p100x100/13501875_1423460301013883_426092165374437510_n.png?oh=23ff635c68eee2faa74b376adc7982fd&oe=58CE1F88",
+ *   "latitude": 43.4642578,
+ *   "longitude": -80.5204096,
+ *   "notes": ""
+ *   },
+ * ...
+ *}
+ */
 @RestController
 public class HackalistHackathonController {
 
